5253bb63d866121ce7d30feeff23b7961a058067
James Provido <jamesprovido555@gmail.com>
2023-07-12 10:29:14 +0800

    First commit - working

---
+++ client.ts
@@ -0,0 +1,16 @@
+// https://www.npmjs.org/package/ws     
+// Starts a WebSocket client and connects to a server
+// console.log(`Hello, WebSockets client!`);
+
+import WebSocket from "ws";
+
+const port = 1234;
+const ws = new WebSocket(`ws://localhost:${port}`);
+
+ws.on('open', () => {
+    console.log(`[Client] Connected.`);
+    ws.send(`Hi, this is a client!`);
+});
+ws.on('message', (data) => {
+     console.log(`Received a message from the server: ${data}`);
+});
